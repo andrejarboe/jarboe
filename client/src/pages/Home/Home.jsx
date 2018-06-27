dbece869074297f0fba3d7cde28f7ab69f1638e8
andrejarboe <andrejarboe@gmail.com>
2018-06-26 22:59:45 -0400

    add background image

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+// import Desk from '../../images/oli-dale-139169-unsplash2.jpg';
+import './Home.css';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <div className="container">
-          <h1>Web and Mobile App Development</h1>
+        <div id="showcase">
+          <div className="container">
+            <h1>Web and Mobile App Development</h1>
+          </div>
         </div>
       </div>
     );
