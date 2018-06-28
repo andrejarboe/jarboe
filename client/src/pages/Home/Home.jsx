938d885b0893e9dfe94e42ac31b870bdca8e212b
andrejarboe <andrejarboe@gmail.com>
2018-06-28 16:59:37 -0400

    add contact button/link

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -14,30 +14,30 @@ class Home extends Component {
 
         {/* Services */}
         <section id="services" className="row">
-        <div className="container">
-          <div className="col s12">
-            <h2>Services</h2>
-            <p>How can we make you happy?</p>
-          </div>
-          <div className="col s12 m4">
-            <h2>Web Design</h2>
-          </div>
-          <div className="col s12 m4">
-            <h2>Mobile Apps</h2>
-          </div>
-          <div className="col s12 m4">
-            <h2>Social Media</h2>
-          </div>
+          <div className="container">
+            <div className="col s12">
+              <h2>Services</h2>
+              <p>How can we make you happy?</p>
+            </div>
+            <div className="col s12 m4">
+              <h2>Web Design</h2>
+            </div>
+            <div className="col s12 m4">
+              <h2>Mobile Apps</h2>
+            </div>
+            <div className="col s12 m4">
+              <h2>Social Media</h2>
+            </div>
           </div>
 
         </section>
 
         {/* Team */}
         <section id="team" className="row">
-        <div className="container">
-          <div className="col s12">
-            <h2>Meet The Team</h2>
-          </div>
+          <div className="container">
+            <div className="col s12">
+              <h2>Meet The Team</h2>
+            </div>
           </div>
 
         </section>
@@ -45,22 +45,22 @@ class Home extends Component {
         {/* Projects */}
         <section id="projects" className="row">
           <div className="container">
-          
-          <div className="col s12">
-            <h2>Some Of Our Projects</h2>
-            <div className="col s12 m6 l3">
-              <h3>Project 1</h3>
+
+            <div className="col s12">
+              <h2>Some Of Our Projects</h2>
+              <div className="col s12 m6 l3">
+                <h3>Project 1</h3>
+              </div>
+              <div className="col s12 m6 l3">
+                <h3>Project 2</h3>
+              </div>
+              <div className="col s12 m6 l3">
+                <h3>Project 3</h3>
+              </div>
+              <div className="col s12 m6 l3">
+                <h3>Project 4</h3>
+              </div>
             </div>
-            <div className="col s12 m6 l3">
-              <h3>Project 2</h3>
-            </div>
-            <div className="col s12 m6 l3">
-              <h3>Project 3</h3>
-            </div>
-            <div className="col s12 m6 l3">
-              <h3>Project 4</h3>
-            </div>
-          </div>
           </div>
 
         </section>
@@ -70,10 +70,12 @@ class Home extends Component {
           <div className="container">
             <div className="col s12">
               <h2>Contact</h2>
+              <p>Don't be shy, get in touch with us</p>
+
+              <a href="/contact" class="waves-effect waves-light btn-large">Contact</a>
             </div>
           </div>
         </section>
-       
 
       </div>
     );
