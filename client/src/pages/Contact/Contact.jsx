1b42342721a59cbe765b163beca350798e91c595
andrejarboe <andrejarboe@gmail.com>
2018-06-28 17:05:32 -0400

    made contact page

---
+++ client/src/pages/Contact/Contact.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Contact.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <section id="contact" className="row">
+          <div className="container">
+            <div className="col s12">
+              <h1>Request A Quote</h1>
+              <p></p>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Home;
