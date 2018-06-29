7d5b3cdf4a8ae6a01a4ebbfed23ab5634f25098c
andrejarboe <andrejarboe@gmail.com>
2018-06-28 20:00:02 -0400

    made contact page and centered text on home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css';
 class Home extends Component {
   render() {
     return (
-      <div>
+      <section id="home">
         <section id="showcase">
           <div className="container">
             <h1>Web, Mobile, and Social Media Development</h1>
@@ -19,12 +19,15 @@ class Home extends Component {
               <p>How can we make you happy?</p>
             </div>
             <div className="col s12 m4">
+              <i className="material-icons">desktop_mac</i>
               <h2>Web Design</h2>
             </div>
             <div className="col s12 m4">
+              <i className="material-icons">phone_iphone</i>
               <h2>Mobile Apps</h2>
             </div>
             <div className="col s12 m4">
+              <i className="material-icons">assessment</i>
               <h2>Social Media</h2>
             </div>
           </div>
@@ -76,7 +79,7 @@ class Home extends Component {
           </div>
         </section>
 
-      </div>
+      </section>
     );
   }
 }
